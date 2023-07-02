5c0372ae30ccbe56c90f24e6d340747011f043c3
Saad <mohammedsaad7866@gmail.com>
2023-07-02 17:11:01 +0530

    Added Solution2

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,53 +2,43 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    protected int price;
     private Boolean isVeg;
-    private boolean extraCheeseAdded;
-    private boolean extraToppingsAdded;
-    private boolean takeawayAdded;
-    private String bill;
+    protected String bill;
 
-    public Pizza(Boolean isVeg){
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
         this.price = isVeg ? 300 : 400;
-        this.extraCheeseAdded = false;
-        this.extraToppingsAdded = false;
-        this.takeawayAdded = false;
-        this.bill = "";
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
-        if (!extraCheeseAdded) {
-            this.price += 80;
-            this.extraCheeseAdded = true;
-            this.bill += "Extra Cheese Added: 80\n";
+    public void addExtraCheese() {
+        if (!bill.contains("Extra Cheese Added")) {
+            this.price += isVeg ? 80 : 0;
+            this.bill += "Extra Cheese Added: " + (isVeg ? 80 : 0) + "\n";
         }
     }
 
-    public void addExtraToppings(){
-        if (!extraToppingsAdded) {
-            this.price += (isVeg ? 70 : 120);
-            this.extraToppingsAdded = true;
+    public void addExtraToppings() {
+        if (!bill.contains("Extra Toppings Added")) {
+            this.price += isVeg ? 70 : 120;
             this.bill += "Extra Toppings Added: " + (isVeg ? 70 : 120) + "\n";
         }
     }
 
-    public void addTakeaway(){
-        if (!takeawayAdded) {
+    public void addTakeaway() {
+        if (!bill.contains("Paperbag Added")) {
             this.price += 20;
-            this.takeawayAdded = true;
             this.bill += "Paperbag Added: 20\n";
         }
     }
 
-    public String getBill(){
-        this.bill = "Base Price Of The Pizza: " + getPrice() + "\n" + this.bill;
-        this.bill += "Total Price: " + getPrice() + "\n";
+    public String getBill() {
+        this.bill += "Total Price: " + this.price + "\n";
         return this.bill;
     }
 }
